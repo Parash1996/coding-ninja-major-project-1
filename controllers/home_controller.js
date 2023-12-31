3bc28ad4c202eff595bd65979c214e610e9de1b3
Parashuram <parashuramchaudhary55@gmail.com>
2023-12-31 12:22:10 +0530

    intorducing views in the code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 // module.exports.actionName=function(req,res){};
 
 module.exports.home=function(req,res){
-    return res.send('<h1>home page is loading for coadial</h1>');
+    // return res.send('<h1>home page is loading for coadial</h1>'); this is using when we are sending data to the brouser i mean when we are not using views.
+ return res.render('home',{
+    title:"Home"
+ })
+     
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.userProfiles=function(req,res){
-    return res.send('<h1>user profile is loading for coadial</h1>');
+    // return res.send('<h1>user profile is loading for coadial</h1>');  tis is using when we are not using the views file
+
+    return res.render('users',{
+        title:"Users"
+    })
 }
