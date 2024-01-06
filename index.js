33b6053983baeb00f0ce28979ffda41c3e1cf164
Parashuram <parashuramchaudhary55@gmail.com>
2024-01-06 16:01:44 +0530

    use of paritals and layouts.

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express =require('express');
-const app=express();
-const port=8000;
+const express = require('express');
+const app = express();
+const port = 8000;
 // use express router here
-const expressLayouts=require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
 app.use(expressLayouts);
+
 app.use(express.static('./assets'))
-app.use('/',require('./routes'));
+
+// extract style and script from sub pageg in to layout.
+app.set('layout extractStyles',true);
+app.set('layout extactScripts', true);
+app.use('/', require('./routes'));
 
 
 //set up view and view engine.
-app.set('view engine','ejs')
-app.set('views','./views')
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 
 
 
-app.listen(port,function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log(`Error running on server :${err}`)
         return
     }
-    else{
+    else {
         console.log(`server is up and running on port:${port}`)
     }
 })
